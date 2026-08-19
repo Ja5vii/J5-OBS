431d2dda936b790353b44d72ba4c19d02e5b3b28
Krynlix <javiernarciso25@gmail.com>
2026-08-19 18:44:56 +0200

    chore: clean up temporary patch scripts

--- remove_disable_plugins.py
+++
@@ -1,9 +0,0 @@
-filepath = r'e:\GitHub\J5-OBS\instance-manager\process_manager.py'
-with open(filepath, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('env["OBS_DISABLE_PLUGINS"] = "1"', '# env["OBS_DISABLE_PLUGINS"] = "1"')
-
-with open(filepath, 'w', encoding='utf-8') as f:
-    f.write(content)
-print("Removed OBS_DISABLE_PLUGINS")
